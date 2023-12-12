2368146acdae5861e51ee0f2035c09fecbbdb74c
HidekiMichi <105813974+HidekiMichi@users.noreply.github.com>
2023-12-12 20:01:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function daysofAYear(year){
+	let isleap = false;
+	if(year%4===0){
+
+		if(year%100===0){
+			return year%400 === 0? 366:365;
+		}
+		return 365;
+	}
+	return 365;
+}
